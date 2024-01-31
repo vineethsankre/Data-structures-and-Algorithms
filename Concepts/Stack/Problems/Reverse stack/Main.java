6963c5a453e4dfae29d0bf97ad80572197210cc5
vineeth2207 <lsvineethsankre@gmail.com>
2024-01-31 20:30:08 +0530

    Reverse stack

--- Concepts/Stack/Problems/Reverse stack/Main.java
+++ Concepts/Stack/Problems/Reverse stack/Main.java
@@ -12,14 +12,22 @@ class Main{
         s.push(temp);
 
     }
+
+    public static void reverse(Stack<Integer> s){
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(s, top);
+
+    }
     public static void main(String[] args) {
 
         Stack <Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-
-        pushAtBottom(s, 4);
 
         while (!s.isEmpty()) {
             System.out.println(s.pop());
