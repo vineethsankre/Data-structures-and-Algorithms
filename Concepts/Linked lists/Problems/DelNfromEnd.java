ae25431d34a35162a6d779d2c8e2b078940e1c86
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-18 17:38:14 +0530

    Remove N node from end

---
+++ Concepts/Linked lists/Problems/DelNfromEnd.java
@@ -0,0 +1,24 @@
+class DelNfromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode firstPointer = dummy;
+        ListNode secondPointer = dummy;
+        
+        for (int i =0; i<n; i++){
+            secondPointer = secondPointer.next;
+        }
+        
+        while(secondPointer.next != null){
+            firstPointer = firstPointer.next;
+            secondPointer = secondPointer.next;
+            
+        }
+        
+        firstPointer.next = firstPointer.next.next;
+        
+        
+        return dummy.next;
+    }
+}
