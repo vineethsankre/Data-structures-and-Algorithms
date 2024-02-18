cf7061e2e8895b10370208be56a0021df474af60
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-18 17:39:56 +0530

    added approach

--- Concepts/Linked lists/Problems/DelNfromEnd.java
+++ Concepts/Linked lists/Problems/DelNfromEnd.java
@@ -1,3 +1,12 @@
+/*
+Initialize two pointers, firstPointer and secondPointer, both initially pointing to a dummy node.
+Move secondPointer n nodes ahead.
+Move both pointers simultaneously until secondPointer reaches the end of the list.
+At this point, firstPointer is pointing to the node before the one to be removed.
+Remove the node by adjusting pointers.
+Return the head of the modified list.
+*/
+
 class DelNfromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(-1);
