1a66f21656075025253771978e2ee098a57d6880
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-19 18:57:40 +0530

    Folder name modified

---
+++ Concepts/HashMap/LongestSubarrayWithEqualZerosOnes.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class LongestSubarrayWithEqualZerosOnes {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxLen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxLen = Math.max(maxLen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxLen;
+    }
+}
