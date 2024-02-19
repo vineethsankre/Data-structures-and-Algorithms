bb358283f18c213eada5baf6dd11aab40526c639
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-19 18:52:12 +0530

    Finding Anagrams in a List of String problem

---
+++ Concepts/Hash table/AnagramGroups.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AnagramGroups {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            java.util.Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
