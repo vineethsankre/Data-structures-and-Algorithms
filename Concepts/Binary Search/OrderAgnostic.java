a21b6c6574b993c2dd895ce9cb072df2563aea01
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-20 22:59:30 +0530

    Binary search updated code

--- Concepts/Binary Search/OrderAgnostic.java
+++ Concepts/Binary Search/OrderAgnostic.java
@@ -13,12 +13,20 @@ public class OrderAgnostic {
             if(target == arr[mid]){
                 return mid;
             }
-
-            if (target < arr[mid]){
-                end = mid - 1;
-            }else if (target > arr[mid]) {
-                start = mid + 1;
+            if (arr[start] < arr[end]){
+                if (target < arr[mid]){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            } else{
+                if (target > arr[mid]){
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
             }
+            
         }
         return - 1; // not found
     }
