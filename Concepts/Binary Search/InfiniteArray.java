320387424de593f76a203bb00dc606b4f75738c6
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-21 20:57:39 +0530

    Infinite Array

--- Concepts/Binary Search/InfiniteArray.java
+++ Concepts/Binary Search/InfiniteArray.java
@@ -5,32 +5,31 @@ public class InfiniteArray {
         System.out.println(ans(arr, target));
     }
     static int ans(int[] arr, int target){
-        
-    }
-    static int binarySearch(int[] arr, int target){
         int start = 0;
-        int end = arr.length - 1;
+        int end = 1;
+        while (target > arr[end]){
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearch(arr, target, start, end);
 
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end){
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if(target == arr[mid]){
-                return mid;
-            }
-            if (arr[start] < arr[end]){
-                if (target < arr[mid]){
-                    end = mid - 1;
-                }else {
-                    start = mid + 1;
-                }
+            
+            if(target < arr[mid]){
+                end = mid - 1;
+            }else if (target > arr[mid]){
+                start = mid + 1;
             } else{
-                if (target > arr[mid]){
-                    end = mid - 1;
-                }else {
-                    start = mid + 1;
-                }
+                return mid;
             }
             
         }
+        
         return - 1; // not found
     }
 }
