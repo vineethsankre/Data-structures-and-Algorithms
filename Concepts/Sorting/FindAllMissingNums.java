e390413fd6c4f8fce2e9742589f2c55e16bb00b0
vineeth2207 <lsvineethsankre@gmail.com>
2024-02-26 20:42:10 +0530

    Find all Missing Numbers modified

--- Concepts/Sorting/FindAllMissingNums.java
+++ Concepts/Sorting/FindAllMissingNums.java
@@ -4,34 +4,32 @@ import java.util.ArrayList;
 public class FindAllMissingNums {
     public static void main(String[] args) {
         int[] arr = {4,3,2,7,8,2,3,1};
-        findAllMissingNums(arr);
+        List<Integer> missingNums = findAllMissingNums(arr);
+        System.out.println("Missing numbers: " + missingNums);
     }
+
     public static List<Integer> findAllMissingNums(int[] nums) {
         int i = 0;
         while (i < nums.length) {
             int correct = nums[i] - 1;
             if (nums[i] != nums[correct]) {
-                swap(nums, i , correct);
+                swap(nums, i, correct);
             } else {
                 i++;
             }
         }
-
-        // just find missing numbers
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> missingNums = new ArrayList<>();
         for (int index = 0; index < nums.length; index++) {
-            if (nums[index] != index+1) {
-                ans.add(index + 1);
+            if (nums[index] != index + 1) {
+                missingNums.add(index + 1);
             }
         }
-
-        return ans;
+        return missingNums;
     }
 
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
-
